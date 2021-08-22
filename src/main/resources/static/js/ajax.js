6ca730b1253dea19854d102b8d4eb0769bfb624f
ben317523 <ben317523@gmail.com>
2021-08-22 17:09:15 +0800

    debug

--- src/main/resources/static/js/ajax.js
+++ src/main/resources/static/js/ajax.js
@@ -33,48 +33,61 @@ function upload(fileId, path, isPublic) {
 function download(fileName, isPublic) {
 
     //Set the File URL.
-    var url = "/download";
-
-    $.ajax({
-        type: 'GET',
-        url: url,
-        data: { param: fileName, isPublic: isPublic },
-        cache: false,
-        xhr: function () {
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 2) {
-                    if (xhr.status == 200) {
-                        xhr.responseType = "blob";
-                    } else {
-                        xhr.responseType = "text";
-                    }
-                }
-            };
-            return xhr;
-        },
-        success: function (data) {
-            //Convert the Byte Data to BLOB object.
-            var blob = new Blob([data], { type: "application/octetstream" });
-
-            //Check the Browser type and download the File.
-            var isIE = false || !!document.documentMode;
-            if (isIE) {
-                window.navigator.msSaveBlob(blob, fileName);
-            } else {
-                var url = window.URL || window.webkitURL;
-                link = url.createObjectURL(blob);
-                var a = $("<a />");
-                a.attr("download", fileName);
-                a.attr("href", link);
-                $("body").append(a);
-                a[0].click();
-                $("body").remove(a);
-            }
-        }
+//    var url = "/download";
+//
+//    $.ajax({
+//        type: 'GET',
+//        url: url,
+//        data: { param: fileName, isPublic: isPublic },
+//        cache: false,
+//        xhr: function () {
+//            var xhr = new XMLHttpRequest();
+//            xhr.onreadystatechange = function () {
+//                if (xhr.readyState == 2) {
+//                    if (xhr.status == 200) {
+//                        xhr.responseType = "blob";
+//                    } else {
+//                        xhr.responseType = "text";
+//                    }
+//                }
+//            };
+//            return xhr;
+//        },
+//        success: function (data) {
+//            //Convert the Byte Data to BLOB object.
+//            var blob = new Blob([data], { type: "application/octetstream" });
+//
+//            //Check the Browser type and download the File.
+//            var isIE = false || !!document.documentMode;
+//            if (isIE) {
+//                window.navigator.msSaveBlob(blob, fileName);
+//            } else {
+//                var url = window.URL || window.webkitURL;
+//                link = url.createObjectURL(blob);
+//                var a = $("<a />");
+//                a.attr("download", fileName);
+//                a.attr("href", link);
+//                $("body").append(a);
+//                a[0].click();
+//                $("body").remove(a);
+//            }
+//        }
+//    });
+    var postData = {
+        param:filename,
+        isPublic:isPublic
+    };
+    var fakeFormHtmlFragment = "<form style='display: none;' method='GET' action='"+"/download"+"'>";
+    _.each(postData, function(postValue, postKey){
+        var escapedKey = postKey.replace("\\", "\\\\").replace("'", "\'");
+        var escapedValue = postValue.replace("\\", "\\\\").replace("'", "\'");
+        fakeFormHtmlFragment += "<input type='hidden' name='"+escapedKey+"' value='"+escapedValue+"'>";
     });
+    fakeFormHtmlFragment += "</form>";
+    $fakeFormDom = $(fakeFormHtmlFragment);
+    $("body").append($fakeFormDom);
+    $fakeFormDom.submit();
 
-    //$.get("/download",{param : fileName,isPublic : isPublic});
 }
 
 function downloadToServer(isPublic) {
