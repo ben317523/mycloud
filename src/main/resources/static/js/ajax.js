eea8fe66bf3a807617c07cc920026641c8a8b92b
ben317523 <ben317523@gmail.com>
2021-08-22 17:00:08 +0800

    debug

--- src/main/resources/static/js/ajax.js
+++ src/main/resources/static/js/ajax.js
@@ -1,8 +1,8 @@
 function upload(fileId, path, isPublic) {
-    $("#main").fadeTo('fast',0.25);
+    $("#main").fadeTo('fast', 0.25);
     $("#loading").show();
     var fd = new FormData();
-    var files = $('#'+fileId)[0].files;
+    var files = $('#' + fileId)[0].files;
 
     if (files.length > 0) {
         fd.append('file', files[0]);
@@ -10,18 +10,19 @@ function upload(fileId, path, isPublic) {
 
         $.ajax({
             type: 'POST',
-            url: "/"+path+"?name=" + $.cookie('name') + "&token=" + $.cookie('token'),
+            url: "/" + path + "?name=" + $.cookie('name') + "&token=" + $.cookie('token'),
             data: fd,
             contentType: false,
             processData: false,
             success: function (response) {
                 $("#loading").hide();
-                $("#main").fadeTo('fast',1);
+                $("#main").fadeTo('fast', 1);
                 if (response != 0) {
                     alert('file uploaded');
                 } else {
                     alert('file not uploaded');
                 }
+                location.reload();
             },
         });
     } else {
@@ -29,61 +30,61 @@ function upload(fileId, path, isPublic) {
     }
 }
 
-function download(fileName,isPublic) {
-function DownloadFile(fileName) {
-            //Set the File URL.
-            var url = "/download" ;
+function download(fileName, isPublic) {
 
-            $.ajax({
-                type : 'GET',
-                url: url,
-                data : {param : fileName,isPublic : isPublic},
-                cache: false,
-                xhr: function () {
-                    var xhr = new XMLHttpRequest();
-                    xhr.onreadystatechange = function () {
-                        if (xhr.readyState == 2) {
-                            if (xhr.status == 200) {
-                                xhr.responseType = "blob";
-                            } else {
-                                xhr.responseType = "text";
-                            }
-                        }
-                    };
-                    return xhr;
-                },
-                success: function (data) {
-                    //Convert the Byte Data to BLOB object.
-                    var blob = new Blob([data], { type: "application/octetstream" });
+    //Set the File URL.
+    var url = "/download";
 
-                    //Check the Browser type and download the File.
-                    var isIE = false || !!document.documentMode;
-                    if (isIE) {
-                        window.navigator.msSaveBlob(blob, fileName);
+    $.ajax({
+        type: 'GET',
+        url: url,
+        data: { param: fileName, isPublic: isPublic },
+        cache: false,
+        xhr: function () {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 2) {
+                    if (xhr.status == 200) {
+                        xhr.responseType = "blob";
                     } else {
-                        var url = window.URL || window.webkitURL;
-                        link = url.createObjectURL(blob);
-                        var a = $("<a />");
-                        a.attr("download", fileName);
-                        a.attr("href", link);
-                        $("body").append(a);
-                        a[0].click();
-                        $("body").remove(a);
+                        xhr.responseType = "text";
                     }
                 }
-            });
-        };
+            };
+            return xhr;
+        },
+        success: function (data) {
+            //Convert the Byte Data to BLOB object.
+            var blob = new Blob([data], { type: "application/octetstream" });
+
+            //Check the Browser type and download the File.
+            var isIE = false || !!document.documentMode;
+            if (isIE) {
+                window.navigator.msSaveBlob(blob, fileName);
+            } else {
+                var url = window.URL || window.webkitURL;
+                link = url.createObjectURL(blob);
+                var a = $("<a />");
+                a.attr("download", fileName);
+                a.attr("href", link);
+                $("body").append(a);
+                a[0].click();
+                $("body").remove(a);
+            }
+        }
+    });
+
     //$.get("/download",{param : fileName,isPublic : isPublic});
 }
 
-function downloadToServer(isPublic){
+function downloadToServer(isPublic) {
     var targetName = $("#targetName").prop("value");
     var url = $("#downloadUrl").prop("value");
-    $.get("/downloadProxy",{url : url,targetName : targetName,isPublic : isPublic,name : $.cookie('name')},function(data,status){
-        if (status == "success"){
+    $.get("/downloadProxy", { url: url, targetName: targetName, isPublic: isPublic, name: $.cookie('name') }, function (data, status) {
+        if (status == "success") {
             console.log(data);
             alert("File Downloaded");
-        }else {
+        } else {
             alert("File Not Downloaded");
         }
     });
@@ -91,11 +92,11 @@ function downloadToServer(isPublic){
 
 function deleteFile(isPublic) {
     var deleteName = $("#deleteName").prop("value");
-    $.post("/delete",{name : deleteName,isPublic : isPublic},function(data,status){
-        if (status == "success"){
+    $.post("/delete", { name: deleteName, isPublic: isPublic }, function (data, status) {
+        if (status == "success") {
             console.log(data);
             alert("File Deleted");
-        }else {
+        } else {
             alert("File Not Deleted");
         }
     });
@@ -103,12 +104,12 @@ function deleteFile(isPublic) {
 
 function makePublic() {
     var privateFileName = $("#privateFileName").prop("value");
-    $.post("/makePublic",{privateFileName : privateFileName},function(data,status){
-        if (status == "success"){
+    $.post("/makePublic", { privateFileName: privateFileName }, function (data, status) {
+        if (status == "success") {
             console.log(data);
             alert("File Become Public");
             location.reload();
-        }else {
+        } else {
             alert("Some Error");
             location.reload();
         }
