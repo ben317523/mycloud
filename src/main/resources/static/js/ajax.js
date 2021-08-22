4ccb967ee35a393b44e7aa7ca20a521b2a38bb5a
ben317523 <ben317523@gmail.com>
2021-08-22 16:48:40 +0800

    debug

--- src/main/resources/static/js/ajax.js
+++ src/main/resources/static/js/ajax.js
@@ -30,8 +30,49 @@ function upload(fileId, path, isPublic) {
 }
 
 function download(fileName,isPublic) {
-    //var fileName = $("#fileName").prop("value");
-    $.get("/download",{param : fileName,isPublic : isPublic});
+function DownloadFile(fileName) {
+            //Set the File URL.
+            var url = "/download" ;
+
+            $.ajax({
+                url: url,
+                data : {param : fileName,isPublic : isPublic},
+                cache: false,
+                xhr: function () {
+                    var xhr = new XMLHttpRequest();
+                    xhr.onreadystatechange = function () {
+                        if (xhr.readyState == 2) {
+                            if (xhr.status == 200) {
+                                xhr.responseType = "blob";
+                            } else {
+                                xhr.responseType = "text";
+                            }
+                        }
+                    };
+                    return xhr;
+                },
+                success: function (data) {
+                    //Convert the Byte Data to BLOB object.
+                    var blob = new Blob([data], { type: "application/octetstream" });
+
+                    //Check the Browser type and download the File.
+                    var isIE = false || !!document.documentMode;
+                    if (isIE) {
+                        window.navigator.msSaveBlob(blob, fileName);
+                    } else {
+                        var url = window.URL || window.webkitURL;
+                        link = url.createObjectURL(blob);
+                        var a = $("<a />");
+                        a.attr("download", fileName);
+                        a.attr("href", link);
+                        $("body").append(a);
+                        a[0].click();
+                        $("body").remove(a);
+                    }
+                }
+            });
+        };
+    //$.get("/download",{param : fileName,isPublic : isPublic});
 }
 
 function downloadToServer(isPublic){
@@ -59,7 +100,7 @@ function deleteFile(isPublic) {
     });
 }
 
-function makePublic() {
+async function makePublic() {
     var privateFileName = $("#privateFileName").prop("value");
     $.post("/makePublic",{privateFileName : privateFileName},function(data,status){
         if (status == "success"){
